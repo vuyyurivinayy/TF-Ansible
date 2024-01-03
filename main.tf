0413521083bceca233963d5b7d81798e8a875cb9
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2024-01-03 11:27:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "aws_launch_configuration" "web_server_as" {
+    image_id           = "ami-00b8917ae86a424c9"
+    instance_type = "t2.micro"
+   
+
+
+  resource "aws_elb" "web_server_lb"{
+     name = "web-server-lb"
+     security_groups = [aws_security_group.web_server.id]
+     subnets = ["ap-south-1a", "ap-south-1b"]
+     listener {
+      instance_port     = 8000
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+    }
+    tags = {
+      Name = "terraform-elb"
+    }
+  }
+resource "aws_autoscaling_group" "web_server_asg" {
+    name                 = "web-server-asg"
+    launch_configuration = aws_launch_configuration.web_server_as.name
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 2
+    health_check_type    = "EC2"
+    load_balancers       = [aws_elb.web_server_lb.name]
+    subnets = ["ap-south-1a", "ap-south-1b"]
+  }
+
