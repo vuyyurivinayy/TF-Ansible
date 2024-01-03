39cbea0c35bf493c74b41bec518e7ffcc9f873e0
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2024-01-03 11:38:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,6 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    subnets = ["ap-south-1a", "ap-south-1b"]
+    
   }
 
