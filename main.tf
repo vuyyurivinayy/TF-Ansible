c83fe6394265c8415e5ab4fcb04e80516873e73d
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2024-03-19 09:21:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-00b8917ae86a424c9"
+    image_id           = "ami-02d7fd1c2af6eead0"
     instance_type = "t2.micro"
-    key_name = "ansible-kp"
+    key_name = "helm"
 }
    
 
