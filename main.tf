934d620f60866fddf5b17926670cca4c0b7965d2
vuyyurivinayy <166721245+vuyyurivinayy@users.noreply.github.com>
2024-06-23 14:44:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_launch_configuration" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-01801d057d263ad13", "subnet-0bcc1a1ec83ddb757"]
+     subnets = ["subnet-098f98971a2ae25bf", "subnet-0ca6f5c2b34776295"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -28,7 +28,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1c"] 
+    availability_zones    = ["us-east-1e", "us-east-1a"] 
     
   }
 
